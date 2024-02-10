8e414738dc692f121dcbc0f5cc2eab604a5eb353
Michael Stein <mstein340@gmail.com>
2024-02-10 12:00:52 -0500

    Misc

--- build/blocks/home--1/index.asset.php
+++ build/blocks/home--1/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ee43f07876009968189a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e3d8e5a39c0adf6ae25f');
