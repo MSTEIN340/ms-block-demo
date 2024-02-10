1a2e2c559ff30df5152da6d00b9624323516fee6
Michael Stein <mstein340@gmail.com>
2024-02-10 11:56:32 -0500

    Fix home page css
    Latest News and Contact Form were not displaying correctly. Adjusted and forced rebuild of all files

--- build/blocks/home--1/index.asset.php
+++ build/blocks/home--1/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cbeb4207e41007e7dd0c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '29cb5a6551ccd8c1dd29');
--- build/blocks/symbiosis/index.asset.php
+++ build/blocks/symbiosis/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dffcede5759c4bca0850');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3cc618bc3118e02747de');
