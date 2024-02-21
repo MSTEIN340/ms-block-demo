39a4ddf50b858b23b4ee4b46a8e17a06cf068ff7
Michael Stein <mstein340@gmail.com>
2024-02-21 14:51:29 -0500

    Add Color Controls to Contact Form

--- src/blocks/ContactForm/contactform.jsx
+++ src/blocks/ContactForm/contactform.jsx
@@ -6,14 +6,18 @@ import { Component } from 'react';
 
 class ContactForm extends Component {
     /* add constructor, add componentdidmount, add state, call AjaxMe from component did mount, and set state to re-render */
-    state = {
 
-    };
 
     scrollMeDiv= null;
 
     constructor(props) {
         super(props);
+        this.state = {
+            borderColor: props.attributes.borderColor,
+            textColor: props.attributes.textColor,
+            backgroundColor: props.attributes.backgroundColor,
+            opacity: props.attributes.opacity,
+        }
     }
 
     componentDidMount() {
@@ -23,11 +27,34 @@ class ContactForm extends Component {
     componentWillUnmount() {
         // Get the div element from the ref
     }
+
+    convertHexToRGBA(hex, opacity) {
+        let r = 0, g = 0, b = 0;
+        if (hex.length === 4) {
+            r = parseInt(hex[1] + hex[1], 16);
+            g = parseInt(hex[2] + hex[2], 16);
+            b = parseInt(hex[3] + hex[3], 16);
+        } else if (hex.length === 7) {
+            r = parseInt(hex[1] + hex[2], 16);
+            g = parseInt(hex[3] + hex[4], 16);
+            b = parseInt(hex[5] + hex[6], 16);
+        }
+        return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    }
+
     render() {
+        const style = {
+            color: this.state.textColor,
+            backgroundColor: this.convertHexToRGBA(this.state.backgroundColor, this.state.opacity),
+            position: 'relative',
+            border: `1px solid ${this.state.borderColor}`, // apply border color
+            textAlign: 'center',
+
+        };
         console.log("Render Contact Form");
         return (
             <>
-                <div className="contact_form-container">
+                <div style={style} className="contact_form-container">
                     <h2>Contact Form</h2>
 
                     <div className="contact_form-row">
--- src/blocks/ContactForm/index.js
+++ src/blocks/ContactForm/index.js
@@ -2,16 +2,153 @@ import { registerBlockType } from '@wordpress/blocks'
 import block from './block.json'
 import icons from '../../icons'
 import ContactForm from './contactform.jsx'
-import './main.css' // This only works because WebPack is running. ( NPM RUN START )
+import './main.css'
+import {InspectorControls, PanelColorSettings} from "@wordpress/block-editor";
+import {PanelBody, RangeControl, TextControl} from "@wordpress/components";
+import {__} from "@wordpress/i18n";
+import {useState} from "@wordpress/element"; // This only works because WebPack is running. ( NPM RUN START )
 
 registerBlockType(block.name, {
-    icon: icons.secondary,
+    icon: icons.Wings,
+    attributes: {
+        postID: {
+            type: 'string',
+            default: ''
+        },
+        link: {
+            type: 'string',
+            default: ''
+        },
+        title: {
+            type: 'string',
+            default: ''
+        },
+        date: {
+            type: 'string',
+            default: ''
+        },
+        author: {
+            type: 'string',
+            default: ''
+        },
+        backgroundColor: {
+            type: 'string',
+            default: '#000000'
+        },
+        borderColor: {
+            type: 'string',
+            default: '#000000'
+        },
+        textColor: {
+            type: 'string',
+            default: '#FFFFFF'
+        },
+        opacity: {
+            type: 'number',
+            default: 1
+        }
+    },
     edit({ attributes, setAttributes }) {
-// Fragment
-        return ( <ContactForm />   )
+        const onTextColorChange = (color) => {
+            setAttributes({ textColor: color });
+            forceRerender();
+        };
+
+        const onBorderColorChange = (color) => {
+            setAttributes({ borderColor: color });
+            forceRerender();
+        };
+
+        const onBackgroundColorChange = (color) => {
+            setAttributes({ backgroundColor: color });
+            forceRerender();
+        };
+
+        const updateOpacity = ( newOpacity ) => {
+            setAttributes( { opacity: newOpacity } );
+            forceRerender();
+        };
+
+
+        const [forceRerenderKey, setForceRerenderKey] = useState(0);
+        const forceRerender = () => {
+            setForceRerenderKey(prevKey => prevKey + 1);
+        };
+        return ( <>
+                <InspectorControls>
+                    <PanelBody title={__("Collapsing Section Settings", "text-domain")}>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            width: '100%',
+                            marginBottom: '5px'
+                        }}>
+                            <div style={{flexGrow: 1, marginRight: '5px'}}>
+                                <TextControl
+                                    label={__("PostID", "text-domain")}
+                                    value={attributes.postID}
+                                    onChange={(value) => onPostIdChange(value)}
+                                    type="string"
+                                />
+
+                            </div>
+                        </div>
+                        <PanelColorSettings
+                            title={__("Text Color", "text-domain")}
+                            initialOpen={true}
+                            colorSettings={[
+                                {
+                                    value: attributes.textColor,
+                                    onChange: (colorValue) => onTextColorChange(colorValue),
+                                    label: __("Text Color", "text-domain"),
+                                },
+                            ]}
+                        />
+                        <PanelColorSettings
+                            title={__("Background Color", "text-domain")}
+                            initialOpen={true}
+                            colorSettings={[
+                                {
+                                    value: attributes.backgroundColor,
+                                    onChange: (colorValue) => onBackgroundColorChange(colorValue),
+                                    label: __("Background", "text-domain"),
+                                },
+                            ]}
+                        />
+                        <RangeControl
+                            label="Opacity"
+                            value={ attributes.opacity }
+                            onChange={ updateOpacity }
+                            min={ 0 }
+                            max={ 1 }
+                            step={ 0.01 }
+                        />
+                        <PanelColorSettings
+                            title={__("Border Color", "text-domain")}
+                            initialOpen={true}
+                            colorSettings={[
+                                {
+                                    value: attributes.borderColor,
+                                    onChange: (colorValue) => onBorderColorChange(colorValue),
+                                    label: __("Border", "text-domain"),
+                                },
+                            ]}
+                        />
+                    </PanelBody>
+                </InspectorControls>
+
+            <ContactForm
+                key={forceRerenderKey}
+                attributes={attributes}
+                postID={attributes.postID}
+            />
+        </>
+        )
     },
     save({attributes}){
-        return ( <ContactForm /> )
+        return ( <ContactForm
+            attributes={attributes}
+        /> )
     }
 })
 
